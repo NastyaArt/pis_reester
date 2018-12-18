73d756c6b194f1ac4ca72f77b1aae9b120630d5d
Artemchuk Anastasia <artemchuk.nastya@gmail.com>
2018-12-18 23:18:49 +0300

    Change input of template

--- src/components/docxTemplater/DocxTemplater.js
+++ src/components/docxTemplater/DocxTemplater.js
@@ -1,47 +1,41 @@
 import React, { Component } from 'react';
 import './DocxTemplater.css';
-import { Button, Input, FormGroup, Form, Label, Col } from "reactstrap";
+import { Button, Input, FormGroup, Form, Label, Col, Alert } from "reactstrap";
 
 import JSZip from 'jszip';
 import Docxtemplater from 'docxtemplater';
 import saveAs from 'file-saver';
 import JSZipUtils from 'jszip-utils';
 
-export const TEMPLATES = {
-    MAIL: "mail.docx",
-    ACT: "act.docx"
-}
-
 class DocxTemplater extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            path: "templates/",
             template: ""
         };
     }
     render() {
-        console.log(this.state.template);
         if (!this.props.data || !this.props.header) {
             return null;
         } else {
             return (
                 <div className="docx-templater-container">
-                    <Form>
+                    <Form onSubmit={this.createTemplate}>
+                        <Alert color="danger" hidden={!this.state.error}>
+                            Ошибка! Файла с таким именем не существует! Проверьте правильность введенного значения.
+                    </Alert>
                         <FormGroup row>
                             <Label sm={4}>Шаблон</Label>
                             <Col sm={8}>
-                                <Input type="select" value={this.state.template} onChange={this.handleChange("template")}>
-                                    <option value="">Выберите шаблон...</option>
-                                    <option value={TEMPLATES.MAIL}>{TEMPLATES.MAIL}</option>
-                                    <option value={TEMPLATES.ACT}>{TEMPLATES.ACT}</option>
-                                </Input>
+                                <Input value={this.state.template} onChange={this.handleChange("template")} placeholder='Введите имя шаблона...' />
                             </Col>
                         </FormGroup>
                         <FormGroup>
                             <h6>Доступные переменные из реестра:</h6>
                             {this.renderTemplateData()}
                         </FormGroup>
-                        <Button onClick={this.createTemplate} disabled={this.state.template === ""}>Сгенерировать файлы по шаблону</Button>
+                        <Button type="submit" disabled={this.state.template === ""}>Сгенерировать файлы по шаблону</Button>
                     </Form>
                 </div>
             )
@@ -66,37 +60,65 @@ class DocxTemplater extends Component {
         return result;
     }
 
-    createTemplate = () => {
+    createTemplate = (e) => {
+        e.preventDefault();
         if (this.state.template === "") {
+            this.setState({ error: true })
             return;
         }
 
-        this.props.data.forEach((elem) => {
-            JSZipUtils.getBinaryContent(this.state.template, (error, content) => {
-                if (error) { throw error };
-                var zip = new JSZip(content);
-                var doc = new Docxtemplater().loadZip(zip);
-                doc.setData(elem);
-                try {
-                    doc.render()
-                }
-                catch (error) {
-                    var e = {
-                        message: error.message,
-                        name: error.name,
-                        stack: error.stack,
-                        properties: error.properties,
-                    }
-                    console.log(JSON.stringify({ error: e }));
-                    throw error;
-                }
-                var out = doc.getZip().generate({
-                    type: "blob",
-                    mimeType: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        this.setState({ error: false })
+
+        let isError = false;
+
+        const doThis = async (data, template) => {
+
+            for (let elem of data) {
+                await new Promise((res, rej) => {
+                    JSZipUtils.getBinaryContent(template, (error, content) => {
+                        if (error) { throw error };
+                        try {
+                            var zip = new JSZip(content);
+                        } catch (err) {
+                            rej();
+                            return;
+                        };
+                        var doc = new Docxtemplater().loadZip(zip);
+                        doc.setData(elem);
+                        try {
+                            doc.render()
+                        }
+                        catch (error) {
+                            var e = {
+                                message: error.message,
+                                name: error.name,
+                                stack: error.stack,
+                                properties: error.properties,
+                            }
+                            console.log(JSON.stringify({ error: e }));
+                            throw error;
+                        }
+                        var out = doc.getZip().generate({
+                            type: "blob",
+                            mimeType: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+                        })
+                        saveAs(out, this.state.template);
+                        res();
+                    });
+                }).catch(() => {
+                    isError = true;
                 })
-                saveAs(out, this.state.template);
-            });
-        })
+
+                if (isError) {
+                    this.setState({ error: true })
+                    return;
+                }
+            }
+        }
+
+        doThis(this.props.data, this.state.path + this.state.template);
+
+        return;
     }
 
 };
